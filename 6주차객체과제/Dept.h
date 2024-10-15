eb78080bbdf6d2482d484e06043a9f6142ca56c9
JinHyeokL <81404484+JinHyeokL@users.noreply.github.com>
2024-10-15 16:38:31 +0900

    Dept.h

--- 6주차객체과제/Dept.h
+++ 6주차객체과제/Dept.h
@@ -2,10 +2,7 @@ class Dept {
 	int size;
 	int* scores;
 public:
-	Dept(int size) {
-		this->size = size;
-		scores = new int[size];
-	}
+	Dept(int size);
 	Dept(const Dept& dept);
 	~Dept();
 	int getSize() { return size; }
